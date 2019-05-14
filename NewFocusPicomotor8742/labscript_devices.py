1d5b135d8e93d7a51f18dfacb04bc37f0d29e40e
pacosalces <pacosalces@localhost>
2019-05-14 15:40:52 -0400

    Microprogress, based on an older version of the labscript_devices.
    Added the control wrapper class PicoMotor8742Controller.py

--- NewFocusPicomotor8742/labscript_devices.py
+++ NewFocusPicomotor8742/labscript_devices.py
@@ -1,6 +1,6 @@
 #####################################################################
 #                                                                   #
-# labscript_devices/NewFocusPicoMotorController.py                  #
+# labscript_devices/NewFocus8742.py                                 #
 #                                                                   #
 # Copyright 2016, Joint Quantum Institute                           #
 #                                                                   #
@@ -20,20 +20,19 @@ __version__ = '1.0.0'
 
 check_version('labscript', '2.5.0', '3.0.0')
 
-from labscript import StaticAnalogQuantity, Device, LabscriptError, set_passed_properties
+from labscript import StaticAnalogOut, Device, LabscriptError, set_passed_properties
 
-#from PicoMotorController_8742 import NewFocus8742
+from .PicoMotor8742Controller import PicoMotor8742Controller
 
-class NewFocus8742Axis(StaticAnalogQuantity):
+class Picomotor(StaticAnalogOut):
     """ Single axis child class """
     min_pos = -2147483648
     max_pos = 2147483647
     description = 'PicoMotorAxis'
 
-class NewFocusPicoMotorController_8742(Device):
+class NewFocus8742(Device):
     description = "NewFocus 8742 controller driver"
-
-    allowed_children = [NewFocus8742Axis]
+    allowed_children = [Picomotor]
 
     @set_passed_properties(
         property_names={
@@ -42,7 +41,7 @@ class NewFocusPicoMotorController_8742(Device):
                 "port",
                 "serial_number",
             ],
-            "device_properties": ["timeout"],
+            "device_properties": [],
         } 
     )
 
@@ -52,33 +51,32 @@ class NewFocusPicoMotorController_8742(Device):
         port=23, 
         serial_number=1, 
         **kwargs
-    ):
-    
-    """ NewFocus Picomotor controller class """
+        ):
 
         self.name = name
         self.host = host
         self.port = port
         self.serial_number = serial_number
         Device.__init__(self, name, None, None, None)
-        self.BLACS_connection = '%s,%d' %(host, slave)
-
+        self.BLACS_connection = '%s,%d' %(host, port)
 
     def generate_code(self, hdf5_file):
         data_dict = {}
-        for motor in self.child_devices:
-            # Call these functions to finalise the motor, they are standard functions of all subclasses of Output:
-            ignore = motor.get_change_times()
+        for motor_axis in self.child_devices:
+            ignore = motor_axis.get_change_times()
             motor.make_timeseries([])
             motor.expand_timeseries()
-            connection = [int(s) for s in motor.connection.split() if s.isdigit()][0]
+            connection = list([int(s) for s in motor.connection.split() if s.isdigit()][0])
             value = motor.raw_output[0]
             if not motor.minval <= value <= motor.maxval:
                 # error, out of bounds
-                raise LabscriptError('%s %s has value out of bounds. Set value: %s Allowed range: %s to %s.'%(motor.description,motor.name,str(value),motor(motor.minval),str(motor.maxval)))
+                raise LabscriptError('%s %s has value out of bounds. \
+                    Set value: %s Allowed range: %s to %s.'%(motor.description,
+                    motor.name,str(value),motor(motor.minval),str(motor.maxval)))
             if not connection > 0 and not connection < 5:
                 # error, invalid connection number
-                raise LabscriptError('%s %s has invalid connection number: %s'%(motor.description,motor.name,str(motor.connection)))
+                raise LabscriptError('%s %s has invalid connection number: %s'%(
+                    motor.description,motor.name,str(motor.connection)))
             data_dict[str(motor.connection)] = value
         dtypes = [(conn, int) for conn in data_dict]
         data_array = np.zeros(1, dtype=dtypes)
