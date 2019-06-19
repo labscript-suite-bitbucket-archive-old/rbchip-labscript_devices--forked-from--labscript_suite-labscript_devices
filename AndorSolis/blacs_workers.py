9ba062c9477f69c1998566ee380146618af1f8d5
pacosalces <pacosalces@localhost>
2019-06-19 14:39:37 -0400

    Added proper interface class to the blacs worker.

--- AndorSolis/blacs_workers.py
+++ AndorSolis/blacs_workers.py
@@ -12,24 +12,55 @@
 #####################################################################
 
 from labscript_devices.IMAQdxCamera.blacs_workers import IMAQdxCameraWorker
-from .andor_sdk.andor_utils import AndorCam
+
+class AndorCamera(object):
+
+    def __init__(self):
+        global AndorCam
+        from .andor_sdk.andor_utils import AndorCam
+        self.camera = AndorCam()
+        self.added_acquisition_attrs = {}
+
+    def set_attributes(self, attr_dict):
+        self.added_acquisition_attrs = attr_dict
+
+    def set_attribute(self, name, value):
+        self.added_acquisition_attrs[name] = value
+
+    def get_attribute_names(self, visibility_level, writeable_only=True):
+        return list(self.added_acquisition_attrs.keys())
+
+    def get_attribute(self, name):
+        try: 
+            self.added_acquisition_attrs[name]
+        except NameError:
+            self.camera.default_acquisition_attrs[name]
+
+    def snap(self):
+        self.camera.snap()
+
+    def configure_acquisition(self, continous=True, bufferCount=3):
+        self.camera.setup_acquisition(self.added_acquisition_attrs)
+
+    def grab(self):
+        return self.camera.grab_acquisition()
+
+    def grab_multiple(self, n_images, images, waitForNextBuffer=True):
+        return self.grab()
+
+    def stop_acquisition(self):
+        pass
+
+    def abort_acquisition(self):
+        self.camera.abort_acquisition()
+
+    def _decode_image_data(self, img):
+        pass
+
+    def close(self):
+        self.camera.shutdown()
 
 class AndorSolisWorker(IMAQdxCameraWorker):
 
-    interface_class = AndorCam
-
-    def get_attributes(self):
-        return self.acquisition_attributes
-
-    def set_attributes(self):
-        pass
-
-    def get_attribute_names(self):
-        return self.acquisition_attributes.keys()
-
-    def get_attribute(self):
-        pass
-
-    def configure_acquisition(self):
-        pass
+    interface_class = AndorCamera
 
